bacbedcd1a1e2f413175cbc679a6f8f7db65acef
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-02-11 19:19:21 -0500

    fixed a bound for sea weed

--- a2/src/main/java/fishtank/Seaweed.java
+++ a2/src/main/java/fishtank/Seaweed.java
@@ -1,21 +1,30 @@
 package fishtank;
+
 import java.awt.*;
 
 /**
  * Seaweed.
  */
 public class Seaweed extends FishTankEntity {
-  /** The font used to draw instances of this class. */
-  static Font FONT = new Font("Monospaced", Font.PLAIN, 10);
+    /**
+     * The font used to draw instances of this class.
+     */
+    static Font FONT = new Font("Monospaced", Font.PLAIN, 10);
 
 
-    /** The number of weed segments. */
+    /**
+     * The number of weed segments.
+     */
     int l;
 
-    /** Indicates whether the bottom segment is leaning right. */
+    /**
+     * Indicates whether the bottom segment is leaning right.
+     */
     boolean leanRight;
 
-    /** My colour. Ah,the vagaries of British vs. US spelling. */
+    /**
+     * My colour. Ah,the vagaries of British vs. US spelling.
+     */
     Color colour;
     private int oriLength;
     private int counter;
@@ -24,121 +33,124 @@ public class Seaweed extends FishTankEntity {
     /**
      * Constructs a new seaweed item at the specified cursor
      * location (x,y),l segments tall.
-     * @param  l  the number of segments this seaweed is tall.
+     *
+     * @param l the number of segments this seaweed is tall.
      */
     public Seaweed(int l) {
-        this.l  =l;
-        colour  =Color.green.darker().darker();
+        this.l = l;
+        colour = Color.green.darker().darker();
         this.oriLength = l;
     }
 
     public int getLenght() {
-      return l;
+        return l;
     }
-
 
 
     /**
      * Draws this fish tank item.  Looks lovely waving in the current, doesn't
      * it?
      *
-     * @param  g  the graphics context in which to draw this item.
+     * @param g the graphics context in which to draw this item.
      */
     void draw(Graphics g) {
 
-     // WWhich way does the first segment lean?
-      boolean lR=leanRight;
+        // WWhich way does the first segment lean?
+        boolean lR = leanRight;
 
-      for (int i=0;i<l;i++) {// Draw a "/" seaweed segment: even numbered and leaning to the right.
-        if ((i%2==0))
-          if (lR)
-            // Draw the string
-            drawString(g,"/",my_curr_row,(-i+my_curr_col));
-      if (i%2==1) // Draw a "/" seaweed segment: odd numbered and leaning to the right.
-        if (lR)
-          // Draw the string
-          drawString(g,"\\",my_curr_row,(-i+my_curr_col));
-      if (i%2==0) // Draw a "/" seaweed segment: even numbered and leaning to the left.
-        if (!lR)
-          // Draw the string
-          drawString(g,"\\",my_curr_row,(-i+my_curr_col));
-      if (i%2==1){ // to make a point about comparing to true or false.
-        if (!lR)
-          // Draw the string for the last kind of leaning of the segment at lcoation  my_curr_row,(-i+my_curr_col)
-          drawString(g,"/",my_curr_row,(-i+my_curr_col));
+        for (int i = 0; i < l; i++) {// Draw a "/" seaweed segment: even numbered and leaning to the right.
+            if ((i % 2 == 0))
+                if (lR)
+                    // Draw the string
+                    drawString(g, "/", my_curr_row, (-i + my_curr_col));
+            if (i % 2 == 1) // Draw a "/" seaweed segment: odd numbered and leaning to the right.
+                if (lR)
+                    // Draw the string
+                    drawString(g, "\\", my_curr_row, (-i + my_curr_col));
+            if (i % 2 == 0) // Draw a "/" seaweed segment: even numbered and leaning to the left.
+                if (!lR)
+                    // Draw the string
+                    drawString(g, "\\", my_curr_row, (-i + my_curr_col));
+            if (i % 2 == 1) { // to make a point about comparing to true or false.
+                if (!lR)
+                    // Draw the string for the last kind of leaning of the segment at lcoation  my_curr_row,(-i+my_curr_col)
+                    drawString(g, "/", my_curr_row, (-i + my_curr_col));
+            }
         }
-      }}
-          /**
+    }
+
+    /**
      * Draws the given string in the given graphics context at
      * at the given cursor location.
      *
-     * @param  g  the graphics context in which to draw the string.
-     * @param  s  the string to draw.
-     * @param  x  the x-coordinate of the string's cursor location.
-     * @param  y  the y-coordinate of the string's cursor location.
+     * @param g the graphics context in which to draw the string.
+     * @param s the string to draw.
+     * @param x the x-coordinate of the string's cursor location.
+     * @param y the y-coordinate of the string's cursor location.
      */
     void drawString(Graphics g, String s, int x, int y) {
         g.setColor(colour);
         g.setFont(FONT);
         FontMetrics fm = g.getFontMetrics(FONT);
-        g.drawString(s, x*fm.charWidth('W'), y*fm.getAscent());
+        g.drawString(s, x * fm.charWidth('W'), y * fm.getAscent());
     }
-
 
 
     /**
      * Set this item's location.
+     *
      * @param a the first coordinate.
-     * @param b  the second coordinate.
+     * @param b the second coordinate.
      */
-    public void setLocation(int a,int b) {
-      this.my_curr_row  =a;
-      this.my_curr_col  =b;
+    public void setLocation(int a, int b) {
+        this.my_curr_row = a;
+        this.my_curr_col = b;
     }
 
-  @Override
-  int getX() {
-    return my_curr_row;
-  }
+    @Override
+    int getX() {
+        return my_curr_row;
+    }
 
-  @Override
-  int getY() {
-    return my_curr_col;
-  }
+    @Override
+    int getY() {
+        return my_curr_col;
+    }
 
     /**
      * Causes this item to take its turn in the fish-tank simulation.
      */
     public void update() {
-        leanRight  =!leanRight;
+        leanRight = !leanRight;
     }
 
-    public void eatCheck(){
-      for ( int i = 0; i <= this.oriLength; i++ ) {
-        if (FishTank.getEntity(this.getX(), this.getY() - i) instanceof Fish || FishTank.getEntity(this.getX(), this.getY() - i) instanceof HungryFish) {
-          this.l = this.oriLength - (this.oriLength - i);
-          System.out.println("found 1 intersection");
-          break;
+    public void eatCheck() {
+        for (int i = 0; i < this.l; i++) {
+            if (FishTank.getEntity(this.getX(), this.getY() - i) instanceof Fish || FishTank.getEntity(this.getX(), this.getY() - i) instanceof HungryFish) {
+                this.l = this.l - (this.l - i);
+                this.counter = 0;
+                break;
+            }
         }
-      }
-      if (this.l < this.oriLength){
-        if (this.counter == 200){
-          this.l += 1;
-          this.counter = 0;
-        }else{
-          this.counter += 1;
+        if (this.l < this.oriLength) {
+            if (this.counter == 200) {
+                this.l += 1;
+                this.counter = 0;
+            } else {
+                this.counter += 1;
+            }
         }
-      }
+
     }
 
 
-
-
-
-
-    /** This bubble's first coordinate. */
+    /**
+     * This bubble's first coordinate.
+     */
     private int my_curr_row;
-    /** This bubble's second coordinate. */
+    /**
+     * This bubble's second coordinate.
+     */
     private int my_curr_col;
 
 
