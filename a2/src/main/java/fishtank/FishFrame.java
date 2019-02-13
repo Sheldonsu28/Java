32791a3124b97a44c04aadc31c7aeb46b9e4af72
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-02-13 01:59:03 -0500

    Reformated FishFrame Code

--- a2/src/main/java/fishtank/FishFrame.java
+++ a2/src/main/java/fishtank/FishFrame.java
@@ -1,4 +1,5 @@
 package fishtank;
+
 import java.awt.*;
 import javax.swing.JFrame;
 
@@ -7,41 +8,44 @@ import javax.swing.JFrame;
  */
 public class FishFrame extends JFrame {
 
-  /** My Serializable id.  */
-  private static final long serialVersionUID = 1409191926708912242L;
+    /**
+     * My Serializable id.
+     */
+    private static final long serialVersionUID = 1409191926708912242L;
 
     /**
      * Paints this fish tank.
      *
-     * @param  g  the graphics context to use for painting.
+     * @param g the graphics context to use for painting.
      */
     public void paint(Graphics g) {
 
         // Get my width and height.
         int w = getBounds().width;
         int h = getBounds().height;
-        
+
         // Paint the window white.
         g.setColor(Color.white);
         g.fillRect(0, 0, w, h);
- 
-			// Tell all the FishTank items to draw themselves.
-            for (int a = 0; a != (int) (480 / 10); a++) {
+
+        // Tell all the FishTank items to draw themselves.
+        for (int a = 0; a != (int) (480 / 10); a++) {
             for (int b = 0; b != (int) (640 / 6); b++) {
-              if (FishTank.getEntity(b, a) != null)
-                  if (FishTank.getEntity(b, a) instanceof Fish) {
-                      ((Fish) FishTank.getEntity(b, a)).draw(g);
-                  }else if (FishTank.getEntity(b, a) instanceof Seaweed) {
-                      ((Seaweed) FishTank.getEntity(b, a)).draw(g);
-                  }else if (FishTank.getEntity(b, a) instanceof HungryFish) {
-                      ((HungryFish) FishTank.getEntity(b, a)).draw(g);
-                  }else if (FishTank.getEntity(b, a) instanceof FollowingFish) {
-                      ((FollowingFish)FishTank.getEntity(b, a)).draw(g);
-                  }if (FishTank.getEntity(b, a) instanceof Bubble) {
-                      ((Bubble) FishTank.getEntity(b, a)).draw(g);
-                  }
+                if (FishTank.getEntity(b, a) != null)
+                    if (FishTank.getEntity(b, a) instanceof Fish) {
+                        ((Fish) FishTank.getEntity(b, a)).draw(g);
+                    } else if (FishTank.getEntity(b, a) instanceof Seaweed) {
+                        ((Seaweed) FishTank.getEntity(b, a)).draw(g);
+                    } else if (FishTank.getEntity(b, a) instanceof HungryFish) {
+                        ((HungryFish) FishTank.getEntity(b, a)).draw(g);
+                    } else if (FishTank.getEntity(b, a) instanceof FollowingFish) {
+                        ((FollowingFish) FishTank.getEntity(b, a)).draw(g);
+                    }
+                if (FishTank.getEntity(b, a) instanceof Bubble) {
+                    ((Bubble) FishTank.getEntity(b, a)).draw(g);
+                }
             }
-            }
+        }
 
     }
 }
