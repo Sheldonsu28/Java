0222323b339e603ddc3997b62a1986dd3d32a717
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-02-17 07:08:27 -0500

    boundary check for Fish test

--- a2/src/test/java/fishtank/FishTest.java
+++ a2/src/test/java/fishtank/FishTest.java
@@ -1,11 +1,15 @@
 package fishtank;
 
+import javafx.beans.binding.When;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class FishTest {
 
@@ -18,10 +22,21 @@ public class FishTest {
        to follow this naming convention!
      */
     private Fish fish;
+    private Seaweed seaweed;
+    private HungryFish hungryFish;
+    private Fish fish2;
+    private FollowingFish followingFish;
+    private Bubble bubble;
 
     @Before
     public void setUp() {
         fish = new Fish();
+        seaweed = new Seaweed(7);
+        fish2 = mock(Fish.class);
+        followingFish = mock(FollowingFish.class);
+        bubble = mock(Bubble.class);
+        hungryFish = mock(HungryFish.class);
+
     }
 
     @Test
@@ -46,17 +61,101 @@ public class FishTest {
         //makes the message much nicer if it fails.
         assertTrue(bubbleMade);
     }
+
+    @Test
+    public void collisionWithSelf(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
+        FishTank.addEntity(32,24, fish);
+        FishTank.addEntity(33,24,fish2);
+        for (int i = 0; i <10; i++){
+            FishTank.addEntity(32,24, fish);
+            fish.goingRight = true;
+            fish.update();
+            assertEquals(32,fish.getX());
+        }
+    }
+    @Test
+    public void collisionWithHungryFish(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
+        FishTank.addEntity(32,24,fish);
+        FishTank.addEntity(33,24,hungryFish);
+        fish.goingRight = true;
+        fish.update();
+        assertEquals(32,fish.getX());
+        assertTrue("fish did not turned around",!fish.goingRight);
+    }
+    @Test
+    public void collisionWithBubble(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
+        FishTank.addEntity(32,24,fish);
+        FishTank.addEntity(33,24,bubble);
+        fish.goingRight = true;
+        fish.update();
+        assertEquals(32,fish.getX());
+        assertTrue("fish did not turned around",!fish.goingRight);
+    }
+    @Test
+    public void collisionWithSeaweed(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
+        FishTank.addEntity(32,24,fish);
+        FishTank.addEntity(33,24,seaweed);
+        fish.goingRight = true;
+        fish.update();
+        assertEquals("fish should not move on X axis",32,fish.getX());
+        assertTrue("fish did not turned around",!fish.goingRight);
+    }
+    @Test
+    public void collisionWithFollowingFish(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
+        FishTank.addEntity(32,24,fish);
+        FishTank.addEntity(33,24,followingFish);
+        fish.goingRight = true;
+        fish.update();
+        assertEquals("fish should not move on X axis",32,fish.getX());
+        assertTrue("fish did not turned around",!fish.goingRight);
+    }
     @Test
     public void alwaysGoingRight(){
-        fish.setLocation(20,10);
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
+        fish.setLocation(0,10);
         for (int i = 0 ; i < 10; i++ ){
             fish.setGoingRight(true);
             fish.update();
+            System.out.println(FishTank.getEntity(fish.getX() + 1,fish.getY()));
         }
-        assertEquals(30, fish.getX());
+        assertEquals(10, fish.getX());
     }
     @Test
     public void alwaysGoingLeft(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
         fish.setLocation(20,10);
         for (int i = 0 ; i < 10; i++ ){
             fish.setGoingRight(false);
@@ -66,42 +165,33 @@ public class FishTest {
     }
 
     @Test
-    public void collisionWithBorder(){
-        fish.setLocation(0,1);
-        for (int i = 0 ; i < 10; i++ ){
-            fish.setGoingRight(false);
-            fish.update();
-        }
-        assertEquals(0, fish.getX());
-
-    }
-
-    @Test
     public void blowBubbleProbability(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
         int counter = 0;
-
         for (int i = 0; i < 1000; i++){
             fish.setLocation(10,11);
             fish.update();
-            for (int x = 0;x < FishTank.getWidth() -1 ; x++){
-                for (int y = 0; y < FishTank.getHeight() - 1; y++){
-                    FishTankEntity e = FishTank.getEntity(x,y);
-                    if (e instanceof Bubble){
-                        counter++;
-                        FishTank.addEntity( x,y, null);
-                    }
-                }
+            FishTankEntity e = FishTank.getEntity(fish.getX(),11);
+            if (e instanceof  Bubble){
+                counter ++;
+                FishTank.entities[fish.getX()][fish.getY()] = null;
             }
-
         }
-        System.out.println(counter);
-        //assertTrue(counter > 50 && counter < 150);
+        assertTrue(counter > 50 && counter < 150);
     }
 
     @Test
     public void moveVerticalProbability(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
         int counter = 0;
-
         for (int i = 0; i< 1000; i++){
             fish.setLocation(10,20);
             fish.update();
@@ -114,6 +204,11 @@ public class FishTest {
     }
     @Test
     public void turnAroundProbability(){
+        for(int x = 0; x < FishTank.width - 1; x++){
+            for(int y  = 0; y < FishTank.height -1 ;y++){
+                FishTank.entities[x][y] = null;
+            }
+        }
         int counter = 0;
         fish.setLocation(32,24);
         for (int i = 0; i< 1000; i++){
@@ -125,5 +220,40 @@ public class FishTest {
         }
         System.out.println(counter);
         assertTrue(counter > 50 && counter < 150);
+    }
+    @Test
+    public void testEatenSeaweedFish(){
+        FishTank.addEntity(23,5,fish);
+        FishTank.addEntity(22,10,seaweed);
+        fish.goingRight = false;
+        fish.update();
+        assertEquals("The length of the seaweed is not correct ",Math.abs(fish.getY() - seaweed.getY()) ,seaweed.l);
+    }
+
+    @Test
+    public void testBoundary(){
+        fish.setLocation(0,11);
+        for(int i = 0;i < 5; i++){
+            fish.goingRight = false;
+            fish.update();
+            assertTrue("Out of bound On left!", fish.getX() >= 0);
+        }
+        fish.setLocation(FishTank.width - 1,11);
+        for(int i = 0;i < 5; i++){
+            fish.goingRight = true;
+            fish.update();
+            assertTrue("Out of bound On right!", fish.getX() <= FishTank.width - 1);
+        }
+        for(int i = 0;i < 1000; i++){
+            fish.setLocation(11,0);
+            fish.update();
+            assertTrue("Out of bound On top!", fish.getY() >= 0);
+        }
+        for(int i = 0;i < 1000; i++){
+            fish.setLocation(11,FishTank.height - 1);
+            fish.update();
+            assertTrue("Out of bound On bottom!", fish.getY() <= FishTank.height - 1 );
+        }
+
     }
 }
