b2ecf8df17504c2c38d13c64d86abd8963766833
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-02-17 09:00:08 -0500

    the true final version of Following Fish Test

--- a2/src/test/java/fishtank/FollowingFishTest.java
+++ a2/src/test/java/fishtank/FollowingFishTest.java
@@ -4,12 +4,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class FollowingFishTest {
   private Fish followee;
   private FollowingFish follower;
+  private HungryFish hungryFish;
+  private HungryFish hungryFish1;
+  private HungryFish hungryFish2;
+  private HungryFish hungryFish3;
+  private Fish fish;
+  private Fish fish1;
+  private Fish fish2;
+  private Fish fish3;
+  private Seaweed seaweed;
+  private Seaweed seaweed1;
+  private Seaweed seaweed2;
+  private Seaweed seaweed3;
+  private Bubble bubble;
+  private Bubble bubble1;
+  private Bubble bubble2;
+  private Bubble bubble3;
+
 
   @Before
   public void setUp() {
@@ -26,6 +44,22 @@ public class FollowingFishTest {
 
     follower = new FollowingFish(followee);
     follower.setLocation(20, 20);
+    fish = mock(Fish.class);
+    fish1 = mock(Fish.class);
+    fish2 = mock(Fish.class);
+    fish3 = mock(Fish.class);
+    hungryFish = mock(HungryFish.class);
+    hungryFish1 = mock(HungryFish.class);
+    hungryFish2 = mock(HungryFish.class);
+    hungryFish3 = mock(HungryFish.class);
+    seaweed = mock(Seaweed.class);
+    seaweed1 = mock(Seaweed.class);
+    seaweed2 = mock(Seaweed.class);
+    seaweed3 = mock(Seaweed.class);
+    bubble = mock(Bubble.class);
+    bubble1 = mock(Bubble.class);
+    bubble2 = mock(Bubble.class);
+    bubble3 = mock(Bubble.class);
   }
 
   @Test
@@ -131,6 +165,50 @@ public class FollowingFishTest {
     int y_dist = Math.abs(follower.getY() - followee.getY());
     assertEquals("X coordinate does not match",1, x_dist);
     assertEquals("Y coordinate does not match",1, y_dist);
+  }
+  @Test
+  public void collisionWithFish(){
+    FishTank.addEntity(22,22,follower);
+    FishTank.addEntity(21,22,fish);
+    FishTank.addEntity(23,22,fish1);
+    FishTank.addEntity(22,23,fish2);
+    FishTank.addEntity(22,21,fish3);
+    follower.goingRight = false;
+    follower.update();
+    assertTrue("Following fish moved!",follower.getX() == 22 && follower.getY() == 22);
+  }
+  @Test
+  public void collisionWithBubble(){
+    FishTank.addEntity(22,22,follower);
+    FishTank.addEntity(21,22,bubble);
+    FishTank.addEntity(23,22,bubble1);
+    FishTank.addEntity(22,23,bubble2);
+    FishTank.addEntity(22,21,bubble3);
+    hungryFish.goingRight = true;
+    hungryFish.update();
+    assertTrue("Following fish moved!",follower.getX() == 22 && follower.getY() == 22);
+  }
+  @Test
+  public void collisionWithSeaweed(){
+    FishTank.addEntity(22,22,follower);
+    FishTank.addEntity(21,22,seaweed);
+    FishTank.addEntity(23,22,seaweed1);
+    FishTank.addEntity(22,23,seaweed2);
+    FishTank.addEntity(22,21,seaweed3);
+    follower.goingRight = true;
+    follower.update();
+    assertTrue("Following fish moved!",follower.getX() == 22 && follower.getY() == 22);
+  }
+  @Test
+  public void collisionWithHungryFish(){
+    FishTank.addEntity(22,22,follower);
+    FishTank.addEntity(21,22,hungryFish);
+    FishTank.addEntity(23,22,hungryFish1);
+    FishTank.addEntity(22,23,hungryFish2);
+    FishTank.addEntity(22,21,hungryFish3);
+    follower.goingRight = true;
+    follower.update();
+    assertTrue("Following fish moved!",follower.getX() == 22 && follower.getY() == 22);
   }
 
 }
